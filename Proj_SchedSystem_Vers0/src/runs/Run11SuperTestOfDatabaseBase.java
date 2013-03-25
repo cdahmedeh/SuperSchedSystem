07b7590132a05e8cfeb41a994478be5a1a72be71
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-03-25 14:59:38 -0400

    Added test for database handler.

---
+++ Proj_SchedSystem_Vers0/src/runs/Run11SuperTestOfDatabaseBase.java
@@ -0,0 +1,27 @@
+package runs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import student.Student;
+
+import database.DatabaseHandler;
+
+public class Run11SuperTestOfDatabaseBase {
+	private static ArrayList<String> courses;
+
+	public static void main(String[] args) {
+	
+		HashMap<Integer, String> classList = new HashMap<>();
+		courses = DatabaseHandler.databaseloadCourses();
+		
+		int i = 0; for (String course: courses){
+			classList.put(i++, course);
+		}
+		
+		ArrayList<Student> students = Run09Generator.generateStudents(20, classList);
+		
+//		DatabaseHandler.databaseSaveCourses(students);
+//		students = DatabaseHandler.databaseloadCourses();
+	}
+}
